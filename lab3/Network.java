8d1161b7295eee10a3cb9eb00d536470b19b0bbd
EliasWal <96151440+EliasWal@users.noreply.github.com>
2023-03-28 18:58:08 +0300

    Update Network.java

--- lab3/Network.java
+++ lab3/Network.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab3;
+import java.util.*;
 
-/**
- *
- * @author Elias
- */
 public class Network {
+    private  List<Node> nodes= new ArrayList<>();
     
+    public void addNode(Node node){
+        nodes.add(node);
+
+    }
+    public List<Node> getNodeList() {
+        return nodes;
+    }
+
+    public void setNodeList(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    //Collections.sort((nodes)network);
+
+    @Override
+    public String toString() {
+        return "Network{" + "nodes=" + nodes + '}';
+    }          
 }
