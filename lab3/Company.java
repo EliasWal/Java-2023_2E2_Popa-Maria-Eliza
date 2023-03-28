a7a36c1ef123ab092c9ce65a522f7bb1084a6c60
EliasWal <96151440+EliasWal@users.noreply.github.com>
2023-03-28 18:57:06 +0300

    Update Company.java

--- lab3/Company.java
+++ lab3/Company.java
@@ -1,35 +1,48 @@
 package lab3;
 import java.util.*;
-/**
- *
- * @author Popa Eliza
- */
-public class Company implements Node {
+
+public class Company implements Node, Comparable<Company>  {
  
     String name;
+    int startYear;
+    int nrEmployee; 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
 
-    @Override
+    public void setNrEmployee(int nrEmployee) {
+        this.nrEmployee = nrEmployee;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public int getNrEmployee() {
+        return nrEmployee;
+    }
+
+    public Company(String name, int startYear, int nrEmployee) {
+        this.name = name;
+        this.startYear = startYear;
+        this.nrEmployee = nrEmployee;
+    }
+
     public String getName() {
         return name;
     }
-    
-    public void setName(String name){
-        this.name=name;
-    }
-    
-    public int compareTo(Person other){
-            return this.name.compareTo(other.name);
-    }
 
     @Override
-    public int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int compareTo ( Company other )
+    {
+        if(!(other instanceof Company))
+            return -1;
+        return this.name.compareTo(other.name);
     }
 
-    @Override
-    public double getWeight() {
-        return Node.super.getWeight(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
+    
 }
