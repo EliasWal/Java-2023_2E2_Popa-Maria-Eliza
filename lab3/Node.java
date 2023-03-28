b1645cfa7c7d08626f1b5ee02aa47bc9b53208d1
EliasWal <96151440+EliasWal@users.noreply.github.com>
2023-03-28 18:58:37 +0300

    Update Node.java

--- lab3/Node.java
+++ lab3/Node.java
@@ -1,16 +1,8 @@
 package lab3;
 import java.util.*;
-/**
- *
- * @author Popa Elizas
- */
+
 public interface Node {
     
-    String getName();
-    
-    int getId();
-    
-    default double getWeight(){
-        return 0.0;
-    }
+    public String getName();
+
 }
