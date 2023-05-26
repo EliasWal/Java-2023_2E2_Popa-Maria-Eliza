668a5d633bf421bcb4a4706b5caaa89ba42957bc
Elias <96151440+EliasWal@users.noreply.github.com>
2023-05-26 21:19:31 +0300

    Add files via upload

--- Lab12/src/main/java/com/mycompany/lab12/Lab12.java
+++ Lab12/src/main/java/com/mycompany/lab12/Lab12.java
@@ -18,6 +18,12 @@ public class Lab12 {
     public static void main(String[] args) throws Exception {
         Loader loader = new Loader();
         loader.loadClassByName("C:\\Users\\Elias\\Desktop\\Facultate\\An II Sem 2\\JAVA\\Lab12\\src\\main\\java\\com\\mycompany\\lab12");
-        
+        System.out.println();
+        loader.getPackages();
+        System.out.println();
+        loader.getMethods();
+        System.out.println();
+        loader.invokeMethods();
+                
     }
 }
--- Lab12/src/main/java/com/mycompany/lab12/Loader.java
+++ Lab12/src/main/java/com/mycompany/lab12/Loader.java
@@ -11,42 +11,49 @@ import java.lang.reflect.InvocationTargetException;
  * @author Elias
  */
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Loader {
+    ClassLoader classLoader;
+    Class<?> clazz;
+    
     public void loadClassByName(String classFilePath) throws Exception {
         File file = new File(classFilePath);
         URL url = file.toURI().toURL();
         URL[] urls = new URL[]{url};
-        ClassLoader classLoader = new URLClassLoader(urls);
-        
+        classLoader = new URLClassLoader(urls);
         String searchedFile = "com.mycompany.lab12.TestClass";
-
-        Class<?> clazz = classLoader.loadClass(searchedFile);
-
-    Package classPackage = clazz.getPackage();
-
+        clazz = classLoader.loadClass(searchedFile);
+    }
+    
+    public void getPackages(){
+        Package classPackage = clazz.getPackage();
         System.out.println("Package: " + classPackage);
-
+    }
+    
+     public void getMethods(){
         StringBuilder stringBuilder = new StringBuilder("Methods: ");
-//        Class<?> clazz = classLoader.loadClass(searchedFile);
         for (Method method : clazz.getMethods()) {
             method.setAccessible(true);
             stringBuilder.append(method.getName()).append(", ");
+        }
+        System.out.println(stringBuilder);
+    }
+     
+    public void invokeMethods(){
+        for (Method method : clazz.getMethods()) {
+            method.setAccessible(true);
             Annotation testAnnotation = method.getDeclaredAnnotation(Test.class);
             if (testAnnotation != null && method.getParameterCount() == 0) {
                 try {
                     Object result = method.invoke(null);
                     System.out.println("Invoked method: " + method.getName());
                     System.out.println("Result: " + result);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
+                } 
+                catch (IllegalAccessException | InvocationTargetException e) {}   
             }
         }
-        System.out.println(stringBuilder);
-
     }
-
+    
+   
 }
